873dbdcfded8f284d0610b581c4c7acae783e0be
marco-soria <marco.soriaa7@gmail.com>
2024-08-07 22:40:32 -0500

    omit and pick

--- src/app/main.ts
+++ src/app/main.ts
@@ -4,7 +4,7 @@ import { addProduct, products } from './products/product.service';
 
 for (let index = 0; index < 50; index++) {
   addProduct({
-    id: faker.string.uuid(),
+    //id: faker.string.uuid(),
     description: faker.commerce.productDescription(),
     image: faker.image.url(),
     color: faker.color.human(),
@@ -13,15 +13,10 @@ for (let index = 0; index < 50; index++) {
     isNew: faker.datatype.boolean(),
     tags: faker.helpers.arrayElements([]),
     title: faker.commerce.productName(),
-    createdAt: faker.date.recent(),
-    updatedAt: faker.date.recent(),
+    //createdAt: faker.date.recent(),
+    //updatedAt: faker.date.recent(),
     stock: faker.number.int({min: 10, max: 100}),
-    category: {
-      id: faker.string.uuid(),
-      name: faker.commerce.department(),
-      createdAt: faker.date.recent(),
-      updatedAt: faker.date.recent(),
-    }
+    categoryId: faker.string.uuid()
   });
 }
 
